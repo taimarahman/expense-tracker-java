be025e6a5c87f5d1188edf18d2115a6f2e3aaa08
Taima Rahman <taimarh@gmail.com>
2025-11-27 23:07:50 +0600

    feat: add global validation exception

--- src/main/java/com/project/expenseTracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/expenseTracker/exception/GlobalExceptionHandler.java
@@ -3,27 +3,33 @@ package com.project.expenseTracker.exception;
 import com.project.expenseTracker.dto.response.ResponseHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UsernameAlreadyExistException.class)
-    public ResponseEntity<Object> handleUsernameAlreadyExistException(UsernameAlreadyExistException ex){
+    public ResponseEntity<Object> handleUsernameAlreadyExistException(UsernameAlreadyExistException ex) {
         return ResponseHandler.generateErrorResponse(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<Object> handleEmailAlreadyExistException(EmailAlreadyExistsException ex){
+    public ResponseEntity<Object> handleEmailAlreadyExistException(EmailAlreadyExistsException ex) {
         return ResponseHandler.generateErrorResponse(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleAllExceptions(Exception ex){
+    public ResponseEntity<Object> handleAllExceptions(Exception ex) {
         ex.printStackTrace();
-        return ResponseHandler.generateErrorResponse("Something went wrong. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseHandler.generateErrorResponse("Something went wrong. Please try again later.",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String firstErrorMessage = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return ResponseHandler.generateErrorResponse(firstErrorMessage, HttpStatus.BAD_REQUEST);
     }
 }
