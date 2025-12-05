ff317cb9af8e6d792dde9b06e6f2aa3d4d495882
Taima Rahman <taimarh@gmail.com>
2025-12-06 02:43:17 +0600

    fix: update response success data

--- src/main/java/com/project/expenseTracker/dto/response/ResponseSuccessData.java
+++ src/main/java/com/project/expenseTracker/dto/response/ResponseSuccessData.java
@@ -1,23 +1,15 @@
 package com.project.expenseTracker.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class ResponseSuccessData {
-    private Boolean status;
-    private Integer statusCode;
-    private String message;
-    private Object data;
 
-    public ResponseSuccessData(Object data, String message, HttpStatus status) {
-        this.status = true;
-        this.statusCode = status.value();
-        this.message = message;
-        this.data = data;
+public class ResponseSuccessData<T> extends ResponseBaseData<T>{
+
+    public ResponseSuccessData(T data, String message, HttpStatus status) {
+        super(data, message, status);
+    }
+
+    public ResponseSuccessData(String message, HttpStatus status) {
+        super(message, status);
     }
 }
